9f4519dff5e25553e454bf5c99790a3a62446ef7
mushood <mushood.badulla@gmail.com>
2018-05-13 21:42:30 +0400

    importing clean blog template
    https://startbootstrap.com/template-overviews/clean-blog/

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,11 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.copy('resources/assets/css/bootstrap.min.css', 'public/css/bootstrap.min.css');
+mix.copy('resources/assets/css/font-awesome.min.css', 'public/css/font-awesome.min.css');
+
+mix.copy('resources/assets/js/bootstrap.bundle.min.js', 'public/js/bootstrap.bundle.min.js');
+mix.copy('resources/assets/js/clean-blog.min.js', 'public/js/clean-blog.min.js');
+mix.copy('resources/assets/js/jquery.min.js', 'public/js/jquery.min.js');
+mix.copy('resources/assets/js/jqBootstrapValidation.min.js', 'public/js/jqBootstrapValidation.min.js');

582e2d8b5acd4fdaf18f6aef542ffa67c99c0245
mushood <mushood.badulla@gmail.com>
2018-05-14 07:23:48 +0400

    update test to match Auth

--- tests/Unit/ArticleTest.php
+++ tests/Unit/ArticleTest.php
@@ -2,15 +2,39 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class ArticleTest extends TestCase
 {
-    public function testCreatePage()
+    use DatabaseTransactions;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        Session::start();
+    }
+
+    public function testCreateNotAccessibleIfNotLoggedInPage()
     {
         $response = $this->get('/article/create');
+        $response->assertStatus(302);
+
+        $response->assertSee('/login');
+    }
+
+    public function testCreatePageAccessibleIfLoggedIn()
+    {
+        $user = User::find(1);
+
+        $response = $this->actingAs($user)->get('/article/create');
         $response->assertStatus(200);
 
         $response->assertSee('Create an article');
